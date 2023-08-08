eb79e50e2d1aad8aceabc1ed214e04961cac4dd1
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-08 07:10:24 -0700

    update README.md

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * check_cycle - a function that checks if a
+ * singly linked list has a syscle in it.
+ *
+ * @list: list to check
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+}
