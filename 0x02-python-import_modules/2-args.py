3705dd4c79ea3f142b1c722e3726f4faff76dbc5
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-11 12:00:31 -0700

    fix error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 
 if __name__ == "__main__":
     import sys
-    if (len(sys.argv) - 1 == 0):
-        print("{} argument{}".format(0, "."))
+    if (len(sys.argv) - 1 == 0:
+        print("{} arguments{}".format(0, "."))
     elif (len(sys.argv) - 1 == 1):
         print("1 argument.")
     else:
         print("{} arguments{}".format(len(sys.argv) - 1, ":"))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
