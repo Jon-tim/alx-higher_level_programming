f218b86162f7c7745bf200a12e4c72e509b64f65
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-13 07:34:38 -0700

    Write a function that adds 2 tuples.

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    first = 0
+    second = 0
+    new_tuple = ()
+
+    if len(tuple_a) == 1 or len(tuple_a) == 0:
+        if tuple_a[0]:
+            tuple_a = (tuple_a[0], 0)
+        else:
+            tuple_a = (0, 0)
+    if len(tuple_b) == 1 or len(tuple_b) == 0:
+        if len(tuple_b) == 1:
+            tuple_b = (tuple_b[0], 0)
+        else:
+            tuple_b = (0, 0)
+
+    first = tuple_a[0] + tuple_b[0]
+    second = tuple_a[1] + tuple_b[1]
+    new_tuple = (first, second)
+
+    return new_tuple
