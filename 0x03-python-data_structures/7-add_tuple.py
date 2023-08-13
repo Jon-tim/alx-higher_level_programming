15d9309423756f69d72d08fe06ba12bc63bf0455
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-13 07:36:42 -0700

    Write a function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,12 +5,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     second = 0
     new_tuple = ()
 
-    if len(tuple_a) == 1 or len(tuple_a) == 0:
-        if tuple_a[0]:
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 1:
             tuple_a = (tuple_a[0], 0)
         else:
             tuple_a = (0, 0)
-    if len(tuple_b) == 1 or len(tuple_b) == 0:
+    if len(tuple_b) < 2:
         if len(tuple_b) == 1:
             tuple_b = (tuple_b[0], 0)
         else:
