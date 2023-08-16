cbbccbc14d8c39a16bf5fa182d1fcfb345a0e984
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-16 10:40:09 -0700

    # Write a function that replaces all occurrences of an element by another in a new list.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-# Write a function that replaces all occurrences of an element by another in a new list.
 
 def search_replace(my_list, search, replace):
-        new_list = list(map(lambda i: replace if i == search else i, my_list))
+    # here is the implementation in LIST COMPREHENSION:
+    # new_list = [replace if i == search else i for i in my_list]
 
+    # here is the implementation using LAMBDA:
+    new_list = list(map(lambda i: replace if i == search else i, my_list))
+    # here is the implementation using a LOOP:
+    # new_list = []
+    # for i in my_list:
+    #     if i == search:
+    #         i = replace
+    #     new_list.append(i)
+
+    return new_list
     return new_list
