79c95f85a5b503c72a5b47a7112e24d38352babb
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-16 16:33:12 -0700

    Write a function that returns a key with the biggest integer value.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 # Write a function that returns a key with the biggest integer value.
 
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
     maximum = list(a_dictionary.keys())[0]
     holder = ""
