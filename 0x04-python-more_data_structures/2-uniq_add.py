b67667d6cd8b114db4ce89aa3724495bcc247a1d
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-16 11:10:14 -0700

    function that adds all unique integers in a list.

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# function that adds all unique integers in a list.
-from functools import reduce
+# function that adds all unique integers in a list
 
 
 def uniq_add(my_list=[]):
