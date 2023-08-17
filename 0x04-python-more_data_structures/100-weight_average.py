f18ef23c6e42fb6311d803ae058b37734a76cb96
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-16 20:02:32 -0700

    Write a function that returns the weighted average of all integers tuple (<score>, <weight>)

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Write a function that returns
+# the weighted average of all integers tuple (<score>, <weight>)
+
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+
+    new = list(map(lambda item: item[0] * item[1], my_list))
+    average = sum(y for _, y in my_list)
+    summation = 0
+
+    for number in new:
+        summation += number
+
+    weighted_average = summation / average
+
+    return weighted_average
