13abb97203e565c4c32593a1c29dd529088f0e5b
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-28 12:24:28 -0700

    safe printing an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    if value:
-        try:
-            n = int(value)
-            print("{:d}".format(n))
-        except ValueError:
-            return False
+     try:
+        n = int(value)
+        print("{:d}".format(n))
+    except ValueError:
+         return False
     return True
