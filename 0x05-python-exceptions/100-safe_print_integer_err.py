c5287182f23f2e7d6897f475ff42652b62aab6d2
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-08-28 21:06:44 -0700

    safe typing

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value), end="\n")
+        return True
+    except:
+        value_type = value.__class__.__name__
+        print("Exception: Unknown format code {} for object of type {}".format(
+            value, value_type), file=sys.stderr)
+        return False
