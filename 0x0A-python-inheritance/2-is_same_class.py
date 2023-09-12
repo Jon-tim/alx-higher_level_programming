10ca853d712b4cff343d164a52dc017f334bc0c1
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-09-11 17:45:15 -0700

    check type of an object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -17,8 +17,7 @@ def is_same_class(obj, a_class):
             of the specified class
             Otherwise False
     """
-    if obj is not None and a_class is not None:
-        if type(obj) is a_class:
-            return True
-        else:
-            return False
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
