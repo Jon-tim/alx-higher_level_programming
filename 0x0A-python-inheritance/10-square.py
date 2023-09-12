b09fcb293b2d177a9f4dea5982f31bb6215f6955
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-09-11 20:24:55 -0700

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A class Square
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class Square that inherits from Rectangle
+    """
+
+    def __init__(self, size):
+        """
+        Instantiation with size
+        """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
