704b8dac458535d27f6e49936ec7ca4542ecde0a
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-09-13 15:06:17 -0700

    fix error

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,8 +15,8 @@ class Square(Rectangle):
         """
         Instantiation with size
         """
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
