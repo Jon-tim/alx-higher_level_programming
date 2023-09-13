09a84bfba5ae7b2d143c783c20c096fcc73cc86a
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-09-13 10:57:03 -0700

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,7 +4,6 @@ Write a function that returns the dictionary description
 with simple data structure (list, dictionary, string, integer
 and boolean) for JSON serialization of an object:
 """
-to_json_string = __import__('3-to_json_string').to_json_string
 
 
 def class_to_json(obj):
