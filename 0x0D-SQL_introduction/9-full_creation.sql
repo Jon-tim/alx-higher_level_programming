3196794cbbd74ff4157bd0b3156a7663f280827e
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-10-17 10:31:59 -0700

    full table creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- creates a table, adds multiple rows
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHARS(256),
+	score INT
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10), (2, 'ALex', 3), (3, 'Bob', 14), (4, 'George', 8);
