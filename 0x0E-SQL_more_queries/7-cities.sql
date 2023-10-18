7863f574a02b36dffcbb94e66bfbd84b10578f9c
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-10-18 08:37:33 -0700

    cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,7 +6,7 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
-	name VARCHAR(256 NOT NULL),
+	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY fk_id(state_id) REFERENCES states(id)
 	);
