3917dcd675a2f1b5d7ee1fbe97b76e6d07a19227
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-10-18 09:26:03 -0700

    number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,12 +1,6 @@
 -- lists the number of shows by genre
-SELECT
-    tv_show_genres.genre AS genre,
-    COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM
-    tv_show_genres
-JOIN
-    tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY
-    tv_show_genres.genre
-ORDER BY
-    number_of_shows DESC;
+SELECT name AS genre, COUNT(genre_id) AS number_shows FROM tv_show_genres
+JOIN tv_genres
+ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_shows DESC; 
