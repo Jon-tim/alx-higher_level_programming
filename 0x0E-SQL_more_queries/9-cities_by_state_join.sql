e8ca02865bf10e0e5634ae60ef914cba4d9053d3
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-10-18 08:58:36 -0700

    cities by states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list cities in the database
-SELECT cities.id, cities.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id 
 ORDER BY cities.id ASC;
