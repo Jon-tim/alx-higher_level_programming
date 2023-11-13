961e334cfe701a900bf0ad785dc3df6ea8869271
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2023-11-13 04:15:00 -0800

    fix logic

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@ const argLength = process.argv.length;
 if (argLength < 3) {
   console.log(0);
 } else {
-  const sorted = process.argv.slice(2).sort();
+  const sorted = process.argv.slice(2).map(Number).sort((a, b) => a - b);
   console.log(sorted[sorted.length - 2]);
 }
