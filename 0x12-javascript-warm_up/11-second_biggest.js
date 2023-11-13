0e28d4403db8fdf16e64524249f9d642e5175709
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2023-11-13 02:40:30 -0800

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argLength = process.argv.length;
+
+if (argLength <= 3) {
+  console.log(0);
+} else {
+  const sorted = process.argv.slice(2).sort();
+  console.log(sorted[sorted.length - 2]);
+}
