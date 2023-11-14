5cdaace506ae8e128946030569de9d5aad1e2d84
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2023-11-14 05:46:01 -0800

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,14 +2,18 @@
 
 const fs = require('fs');
 
-fs.readFile('fileA', 'utf-8', (firstErr, firstData) => {
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+fs.readFile(fileA, 'utf-8', (firstErr, firstData) => {
   if (firstErr) {
     console.error(firstErr);
     return;
   }
   const textOne = firstData;
 
-  fs.readFile('fileB', 'utf-8', (secondErr, secondData) => {
+  fs.readFile(fileB, 'utf-8', (secondErr, secondData) => {
     if (secondErr) {
       console.error(secondErr);
       return;
@@ -18,7 +22,7 @@ fs.readFile('fileA', 'utf-8', (firstErr, firstData) => {
 
     const textThree = textOne + '\n' + textTwo + '\n';
 
-    fs.writeFile('fileC', textThree, (err) => {
+    fs.writeFile(fileC, textThree, (err) => {
       if (err) {
         console.error(err);
       }
