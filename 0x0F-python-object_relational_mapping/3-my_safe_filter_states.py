5a2e17458352f8f1749155c86a2f8e41760e4dc0
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-11-17 06:38:37 -0800

    filtered safe states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A script that lists all states with a name starting
+with N (uppercase) from the database"""
+
+
+def main():
+    """create a connection and get all states with 'N'"""
+    import MySQLdb
+    import sys
+
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    pswd = sys.argv[2]
+    db = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(host=host, user=user, passwd=pswd, db=db, port=port)
+    cursor = db.cursor()
+    query = """SELECT * FROM states
+                   WHERE states.name LIKE BINARY %s
+                   ORDER BY states.id ASC;"""
+
+    cursor.execute(query, (state,))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
