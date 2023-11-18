9ce08796bb1226fad39b18718a6698bce0d8f432
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-11-18 02:12:02 -0800

    create model state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+"""A python file that contains the class definition
+of a State and an instance Base = declarative_base()"""
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """A base class"""
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
