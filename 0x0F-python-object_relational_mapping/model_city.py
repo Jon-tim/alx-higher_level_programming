1fda8baace945f029d7c86bf4e5b34747ed3898a
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-11-18 07:49:09 -0800

    import State

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,7 +4,7 @@ of a City"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from model_state import Base
+from model_state import Base, State
 
 
 class City(Base):
@@ -14,4 +14,4 @@ class City(Base):
                 primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
+    state = relationship(State, back_populates="cities")
