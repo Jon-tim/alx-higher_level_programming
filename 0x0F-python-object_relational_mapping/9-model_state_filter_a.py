af3a40e938847bd6851d409ea88c785964d41a28
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-11-18 03:55:30 -0800

    show states that contain a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""a script that lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa"""
+
+
+def main():
+    """starting point"""
+    import sys
+    from sqlalchemy.orm import sessionmaker, Session
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    pswd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}"
+                           .format(user, pswd, host, port, db))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.name.like("%a%")).all()
+
+    if query:
+        for state in query:
+            print('{}: {}'.format(state.id, state.name))
+    # else:
+    #     print("Nothing")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
