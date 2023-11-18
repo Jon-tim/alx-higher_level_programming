fa07b90eea7291c712617b708088432a2ccab46a
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-11-18 11:16:15 -0800

    get cities

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-
 """ A Script that lists all cities by state
 from the database hbtn_0e_0_usa"""
 
 
 def main():
-    """create a connection and get all states"""
-    import MySQLdb
+    """create a connection and get all cities"""
     import sys
-    from model_state import Base, State
+    from model_state import State
+    from model_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
 
     host = "localhost"
     port = 3306
@@ -16,23 +17,17 @@ def main():
     pswd = sys.argv[2]
     db = sys.argv[3]
 
-    db = MySQLdb.connect(host=host, user=user, passwd=pswd, db=db, port=port)
-    cursor = db.cursor()
+    engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(
+        user, pswd, host, port, db), pool_pre_ping=True)
 
-    query = """SELECT cities.id, cities.name, states.name
-                   FROM cities
-                   JOIN {}
-                   ON cities.state_id = states.id
-                   ORDER BY cities.id ASC;""".format(State.__table__.name)
-    cursor.execute(query)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    rows = cursor.fetchall()
-    for row in rows:
-        print('{}: ({}) {}'.format(
-            row[2], row[0], row[1]))
+    query = session.query(City, State).filter(
+        City.state_id == State.id).order_by(City.id)
 
-    cursor.close()
-    db.close()
+    for city, state in query:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
 
 if __name__ == "__main__":
