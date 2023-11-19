79ea5f11f026632b59ee8fe8e97fdfc2a573c872
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-11-19 08:02:11 -0800

    from city

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""a script that lists all City objects from the
+database hbtn_0e_101_usa"""
+
+
+def main():
+    """starting point"""
+    import sys
+    from relationship_state import State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    pswd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(
+        user, pswd, host, port, db), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(City).order_by(City.id).all()
+
+    for city in query:
+        print('{}:{} -> {}'.format(city.id, city.name, city.state.name))
+    session.close()
+
+
+if __name__ == '__main__':
+    main()
