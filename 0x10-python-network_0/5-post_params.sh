f6a4f3d0c0e1540cf48795e9eabcd41eab8fe475
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-11-30 08:33:15 -0800

    POST Parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request, and displays the response
-curl -s -X POST -d "email=test@gmail&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -sX POST -d "email=test@gmail&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
