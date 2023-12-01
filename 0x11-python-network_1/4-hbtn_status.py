a3110cf35a3169e58499c401cd7bc426c68e5221
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-12-01 11:40:11 -0800

    requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    req = requests.get(url)
+    res = req.text
+    print(
+        f"Body response:\n"
+        f"\t- type: {type(res)}\n"
+        f"\t- content: {res}\n"
+    )
