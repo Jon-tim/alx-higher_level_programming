806f3d026771f0e8d01092a0a10e2b9db118ff2c
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2023-12-02 09:08:01 -0800

    list 10 commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos"
     req = requests.get(f"{url}/{owner}/{repo}/commits")
     res = req.json()
-    for commit in range(1, 11):
-        sha = res[commit].get('sha')
-        author = res[commit].get('commit').get('author').get('name')
+    for commit in res[:10]:
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
         print(f"{sha}: {author}")
