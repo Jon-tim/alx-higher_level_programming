871b482989cc8d724e29f2272cd26d62d8a3e1fd
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-12-01 23:06:15 -0800

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,8 +8,6 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     req = requests.get(url)
     res = req.text
-    print(
-        f"Body response:\n"
-        f"\t- type: {type(res)}\n"
-        f"\t- contenti: {res}"
-    )
+    print("Body response:")
+    print(f"\t- type: {type(res)}")
+    print(f"\t- contenti: {res}")
