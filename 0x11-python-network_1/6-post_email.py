047a0c958a0623c454f7423bbd17d7c35093a630
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-12-01 23:51:29 -0800

    post an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+import requests
+import sys
+
+url = sys.argv[1]
+mail = sys.argv[2]
+payload = {'email': mail}
+req = requests.post(url, data=payload)
+response = req.text
+print(response)
