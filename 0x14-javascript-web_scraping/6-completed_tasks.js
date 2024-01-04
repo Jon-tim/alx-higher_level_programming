50430621e25d3a252ab33e1b5eec0d16fea6fc1f
oluwatimilehin awoniyi <awoniyitimi@gmail.com>
2024-01-03 18:19:17 -0800

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,11 +5,10 @@ const API = process.argv[2];
 
 request.get(API, function (response, body) {
   const result = JSON.parse(body.body);
-  let doneTask = 0;
   const data = {};
   for (let i = 1; i <= 10; i++) {
-    doneTask = 0;
-    for (let j = 0; j <= result.length - 1; j++) {
+    let doneTask = 0;
+    for (let j = 0; j < result.length; j++) {
       if (Number(result[j].userId) === i && result[j].completed) {
         doneTask += 1;
       }
