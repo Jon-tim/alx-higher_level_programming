aee5db0c7fdf9af0cc37de9c84e1899ad14b4f36
oluwatimilehin awoniyi <awoniyitimi@gmail.com>
2024-01-03 17:19:52 -0800

    get Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,11 +2,12 @@
 
 const request = require('request');
 const address = process.argv[2];
+const targetID = '18';
 
 request.get(address, function (response, body) {
   const result = JSON.parse(body.body).results;
   const wedgeAntillesPresent = result.filter(film =>
-    film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    film.characters.some(character => character.includes(targetID))
   );
   console.log(wedgeAntillesPresent.length);
 });
